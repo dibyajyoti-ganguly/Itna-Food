f9286fd0dc734dde22089ef7396008a2f243989e
Dibyajyoti Ganguly <gangulyd49@gmail.com>
2024-10-17 12:32:59 +0530

    Built the mockup navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import Logo from "./images/Group 18116.png"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const Header = ()=>{
+  return(
+    <div className='header'>
+      <div className='logo-container'>
+        <img className="logo" src={Logo} alt="logo"></img>
+      </div>
+      <div className='nav-items'>
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>  
     </div>
-  );
+  )
 }
+
+
+function App(){
+  return (
+    <div className='app'>
+      <Header/>
+
+    </div>
+  )
+}
+
+
+
 
 export default App;

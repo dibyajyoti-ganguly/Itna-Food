1c861666c1e68a9519802a2c963af1a5bbe001ed
Dibyajyoti Ganguly <gangulyd49@gmail.com>
2024-10-20 10:01:24 +0530

    Reviewed the code and made it cleaner by creating distinct functional components

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import Logo from "../images/Group 18116.png";
+
+const Header = () => {
+    return (
+      <div className="header">
+        <div className="logo-container">
+          <img className="logo" src={Logo} alt="logo"></img>
+        </div>
+        <div className="search"></div>
+        <div className="nav-items">
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Contact Us</li>
+            <li>Cart</li>
+          </ul>
+        </div>
+      </div>
+    );
+  };
+
+export default Header;

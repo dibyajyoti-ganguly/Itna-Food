6e0feea24f0c86c7088ad04da22fb8190a82c936
Dibyajyoti Ganguly <gangulyd49@gmail.com>
2024-11-09 10:29:08 +0530

    Made the resturant cards dynamic by fetching the restaurant list data from the API and redndering on the cards

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,36 +1,29 @@
+import CDN_URL from "../utils/constants";
 
 const RestaurantCard = (props) => {
-    const { resData } = props;
-    const {
-      name,
-      cloudinaryImageId,
-      avgRating,
-      sla,
-      cuisines,
-      locality,
-      areaName,
-    } = resData.info;
-    console.log(resData);
-    return (
-      <div className="res-card">
-        <img
-          src={
-            "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
-            cloudinaryImageId
-          }
-          alt="resimg"
-        />
-        <h3>{name}</h3>
-        <h4>
-          ⭐{avgRating} • {sla.slaString}
-        </h4>
-        <p>{cuisines.slice(0, 2).join(", ")}</p>
-        <p>
-          {locality}, {areaName}
-        </p>
-      </div>
-    );
-  };
+  const { resData } = props;
+  const {
+    name,
+    cloudinaryImageId,
+    avgRating,
+    sla,
+    cuisines,
+    locality,
+    areaName,
+  } = resData.info;
+  return (
+    <div className="res-card">
+      <img src={CDN_URL + cloudinaryImageId} alt="resimg" />
+      <h3>{name}</h3>
+      <h4>
+        ⭐{avgRating} • {sla.slaString}
+      </h4>
+      <p>{cuisines.slice(0, 2).join(", ")}</p>
+      <p>
+        {locality}, {areaName}
+      </p>
+    </div>
+  );
+};
 
-
-  export default RestaurantCard;
+export default RestaurantCard;

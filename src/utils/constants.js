51eb92aa2add01cd960ada468b672d7c454b7761
Dibyajyoti Ganguly <gangulyd49@gmail.com>
2024-12-15 11:52:45 +0530

    Modified the APi to send list of restaurants in Bangalore

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,9 +2,9 @@ export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 export const LIST_URL =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.4449799&lng=78.3596892&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532";
 
 export const MENU_URL =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.4449799&lng=78.3596892&restaurantId=";
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.572234&lng=88.4146371&restaurantId=";
 
 export const USER_URL = "https://api.github.com/users/dibyajyoti-ganguly";

90460f177e88d6e4424b757f43db29ae5e5d5433
Dibyajyoti Ganguly <gangulyd49@gmail.com>
2024-12-22 12:20:57 +0530

    Implemnted an accordion functionality in all the item catgeories

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,47 +1,18 @@
-import { CDN_URL } from "../utils/constants";
+import ItemList from "./ItemList";
+import { useState } from "react";
 
 const RestaurantCategory = ({ itemCards, title }) => {
+  const [click, setClick] = useState(0);
+
   return (
     <div>
-      <h2 className="text-xl font-extrabold my-5">{title}</h2>
-      <ul key={title}>
-        {" "}
-        {itemCards?.map((item) => {
-          return (
-            <li
-              className="flex w-[680px] justify-between list-none mb-10"
-              key={item?.card?.info?.id}
-            >
-              <span className="w-[470px] text-[rgba(2,6,12,0.6)]">
-                <p className="font-extrabold text-black">
-                  {item?.card?.info?.name}
-                </p>
-                <p className="font-semibold text-slate-950">
-                  ₹
-                  {(
-                    (item?.card?.info?.price ||
-                      item?.card?.info?.defaultPrice) / 100
-                  ).toFixed(0)}
-                </p>
-                {Object.keys(item?.card?.info?.ratings?.aggregatedRating)
-                  .length ? (
-                  <p className="font-medium text-slate-950">
-                    ⭐{item?.card?.info?.ratings?.aggregatedRating?.rating}(
-                    {item?.card?.info?.ratings?.aggregatedRating?.ratingCountV2}
-                    )
-                  </p>
-                ) : null}
-                <p className="line-clamp-2">{item?.card?.info?.description}</p>
-              </span>
-              <img
-                className="w-40 h-36 rounded-xl shadow-2xl"
-                src={CDN_URL + item?.card?.info?.imageId}
-                alt="Item"
-              />
-            </li>
-          );
-        })}
-      </ul>
+      <div className="flex justify-between w-[680px]">
+        <h2 className="text-xl font-extrabold my-5">
+          {title}({itemCards.length})
+        </h2>
+        <button onClick={() => setClick(!click)}>{click ? "▲" : "▼"}</button>
+      </div>
+      {click ? <ItemList itemCards={itemCards} title={title} /> : null}
       <hr className="border-t-2 border-gray-300" />
     </div>
   );
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@ export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 export const LIST_URL =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.4435653&lng=78.3603021";
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.572234&lng=88.4146371";
 
 export const MENU_URL =
   "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.572234&lng=88.4146371&restaurantId=";
